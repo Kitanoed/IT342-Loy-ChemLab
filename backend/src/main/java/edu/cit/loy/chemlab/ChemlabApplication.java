2cb035c90c3a9f10ed68ad9e436e3053065bb412
Kitanoed <L13Y09W24@LabHCI.net>
2026-03-07 08:32:11 +0800

    setup: backend setup using springboot

---
+++ backend/src/main/java/edu/cit/loy/chemlab/ChemlabApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.loy.chemlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChemlabApplication.class, args);
+	}
+
+}
